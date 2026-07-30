0642dd249b96fc3396cab90fe5747466ba12ef35
1F10250255 <s1f102502553@iniad.org>
2026-07-30 17:59:03 +0900

    test: add task service tests

--- week1/src/taskService.test.ts
+++ week1/src/taskService.test.ts
@@ -1,15 +1,49 @@
 import { beforeEach, describe, expect, test } from "vitest";
-import { addTask, initializeTasks } from "./taskService.js";
+import { addTask, completeTask, getTasks, initializeTasks } from "./taskService.js";
+
+beforeEach(() => {
+  initializeTasks([]);
+});
 
 describe("addTask", () => {
-  beforeEach(() => {
-    initializeTasks([]);
-  });
 
   test("新しいtodoタスクを作成する", () => {
     const task = addTask({ title: "test" });
+
     expect(task.id).toBe(1);
     expect(task.title).toBe("test");
     expect(task.status).toBe("todo");
   });
+
+  test("タスクを追加するたびにIDが増える", () => {
+    const task1 = addTask({ title: "test1" });
+    const task2 = addTask({ title: "test2" });
+
+    expect(task1.id).toBe(1);
+    expect(task2.id).toBe(2);
+  });
+});
+
+describe("getTasks", () => {
+
+  test("タスク配列を取得", () => {
+    const task1 = addTask({ title: "test1" });
+    const task2 = addTask({ title: "test2" });
+
+    expect(getTasks().length).toBe(2);
+  });
+});
+
+describe("completeTask", () => {
+
+  test("指定したタスクを完了状態にする", () => {
+    const task1 = addTask({ title: "test1" });
+    const completedTask = completeTask(task1.id);
+    expect(completedTask).toBeDefined();
+    expect(completedTask?.status).toBe("done");
+  });
+
+  test("存在しないIDを指定した場合、undefinedを返す", () => {
+    expect(completeTask(100)).toBeDefined();
+  })
 });
